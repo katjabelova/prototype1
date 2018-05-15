230494f2bdf40f1ed0f12a0246432f6cbd282cc6
katjabelova <katja.belova@googlemail.com>
2018-05-15 11:36:58 +0200

    pager added

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
 
+  def admin_view
+      @pageNumber = 0
+      @users = User.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def index
+    @admins = Users.paginate(:page => params[:page], :per_page => 5)
+  end
+
   def destroy
     UserRole.where(user_id: params[:id]).destroy_all
     User.find(params[:id]).destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def index
+    @pageNumber = 0
+    @users = User.paginate(:page => params[:page], :per_page => 5)
+  end
+
   def new
     @user = User.new
   end
